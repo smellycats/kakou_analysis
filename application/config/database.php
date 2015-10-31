9142eb2bd43e4b834a557790cc7b52d56fccbb29
SmellyCat <smellycat2014@foxmail.com>
2015-10-31 16:04:58 +0800

    Remove cgs_db

--- application/config/database.php
+++ application/config/database.php
@@ -84,25 +84,4 @@ $db['default'] = array(
 	'save_queries' => TRUE
 );
 
-$db['cgs_db'] = array(
-	'dsn'	=> '',
-	'hostname' => '127.0.0.1',
-	'username' => 'root',
-	'password' => 'root',
-	'database' => 'cgs',
-	'dbdriver' => 'mysqli',
-	'dbprefix' => '',
-	'pconnect' => FALSE,
-	'db_debug' => TRUE,
-	'cache_on' => FALSE,
-	'cachedir' => '',
-	'char_set' => 'utf8',
-	'dbcollat' => 'utf8_general_ci',
-	'swap_pre' => '',
-	'encrypt' => FALSE,
-	'compress' => FALSE,
-	'stricton' => FALSE,
-	'failover' => array(),
-	'save_queries' => TRUE
-);
  
